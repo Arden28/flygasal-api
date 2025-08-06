24466af724dfd35cfbce25bf7c1ceeb7fb42f296
Arden28 <laudbouetoumoussa@gmail.com>
2025-08-06 10:54:42 +0100

    Updated booking creation logic

--- app/Http/Controllers/Api/FlightController.php
+++ app/Http/Controllers/Api/FlightController.php
@@ -125,6 +125,10 @@ class FlightController extends Controller
             // 3. Call PKfareService to precise pricing
             $precisePricing = $this->pkfareService->getPrecisePricing($criteria);
 
+            // 4. Call PKfareService to ancillary pricing
+            $ancillaryPricing = $this->pkfareService->ancillaryPricing($criteria);
+            Log::info('Ancillary: ', $ancillaryPricing['data']);
+
             // 4. Return successful response with flight data
             return response()->json([
                 'message' => 'Precise pricing retrieved successfully.',
