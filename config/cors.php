844024a9b3f1cfd28b7d08fd2705fd39895a2042
Arden28 <laudbouetoumoussa@gmail.com>
2025-11-23 22:29:23 +0100

    Added flygasal.net to cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'https://flygasal.vercel.app'],
+    'allowed_origins' => ['http://localhost:5173', 'https://flygasal.vercel.app', 'https://flygasal.net'],
 
     'allowed_origins_patterns' => [],
 
